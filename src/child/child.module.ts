e104193ddb0b837ca38f938fce08d15a753a1b68
steveroseik <steveroseik@aucegypt.edu>
2025-02-22 14:25:40 +0200

    setup child module imports & exports

--- src/child/child.module.ts
+++ src/child/child.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ChildService } from './child.service';
 import { ChildResolver } from './child.resolver';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Child } from './entities/child.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Child])],
   providers: [ChildResolver, ChildService],
+  exports: [ChildService],
 })
 export class ChildModule {}
