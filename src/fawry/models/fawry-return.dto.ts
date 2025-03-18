65245d8bec506b942e5efdd3b8241d8cd22bca96
steveroseik <steveroseik@aucegypt.edu>
2025-03-19 00:00:32 +0200

    fix fawry payload

--- src/fawry/models/fawry-return.dto.ts
+++ src/fawry/models/fawry-return.dto.ts
@@ -1,63 +1,80 @@
-import { IsString, IsNumber, IsBoolean } from 'class-validator';
+import { IsString, IsNumber, IsBoolean, IsOptional } from 'class-validator';
 import { Type } from 'class-transformer';
 
 export class FawryReturnDto {
   @IsString()
-  type: string;
+  @IsOptional()
+  type?: string;
 
   @IsString()
-  referenceNumber: string;
+  @IsOptional()
+  referenceNumber?: string;
+
+  @IsNumber()
+  @IsOptional()
+  @Type(() => Number)
+  merchantRefNumber?: number;
+
+  @IsNumber()
+  @IsOptional()
+  @Type(() => Number)
+  orderAmount?: number;
+
+  @IsNumber()
+  @IsOptional()
+  @Type(() => Number)
+  paymentAmount?: number;
+
+  @IsNumber()
+  @IsOptional()
+  @Type(() => Number)
+  fawryFees?: number;
+
+  @IsString()
+  @IsOptional()
+  orderStatus?: string;
+
+  @IsString()
+  @IsOptional()
+  paymentMethod?: string;
 
   @IsNumber()
   @Type(() => Number)
-  merchantRefNumber: number;
+  @IsOptional()
+  paymentTime?: number;
+
+  @IsString()
+  @IsOptional()
+  cardLastFourDigits?: string;
+
+  @IsString()
+  @IsOptional()
+  customerName?: string;
+
+  @IsString()
+  @IsOptional()
+  customerProfileId?: string;
+
+  @IsString()
+  @IsOptional()
+  signature?: string;
+
+  @IsNumber()
+  @IsOptional()
+  @Type(() => Number)
+  taxes?: number;
 
   @IsNumber()
   @Type(() => Number)
-  orderAmount: number;
-
-  @IsNumber()
-  @Type(() => Number)
-  paymentAmount: number;
-
-  @IsNumber()
-  @Type(() => Number)
-  fawryFees: number;
+  @IsOptional()
+  statusCode?: number;
 
   @IsString()
-  orderStatus: string;
-
-  @IsString()
-  paymentMethod: string;
-
-  @IsNumber()
-  @Type(() => Number)
-  paymentTime: number;
-
-  @IsString()
-  cardLastFourDigits: string;
-
-  @IsString()
-  customerName: string;
-
-  @IsString()
-  customerProfileId: string;
-
-  @IsString()
-  signature: string;
-
-  @IsNumber()
-  @Type(() => Number)
-  taxes: number;
-
-  @IsNumber()
-  @Type(() => Number)
-  statusCode: number;
-
-  @IsString()
-  statusDescription: string;
+  @IsOptional()
+  statusDescription?: string;
 
   @IsBoolean()
   @Type(() => Boolean)
-  basketPayment: boolean;
+  @IsOptional()
+  basketPayment?: boolean;
 }
