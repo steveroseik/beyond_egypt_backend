14590bb1770af0d13430cb1c5c04aa804d5e03ea
steveroseik <steveroseik@aucegypt.edu>
2025-05-23 21:29:20 +0300

    resolve fields for registration attendance

--- src/registration-attendance/registration-attendance.resolver.ts
+++ src/registration-attendance/registration-attendance.resolver.ts
@@ -1,4 +1,13 @@
-import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
+import {
+  Resolver,
+  Query,
+  Mutation,
+  Args,
+  Int,
+  ResolveField,
+  Parent,
+  Context,
+} from '@nestjs/graphql';
 import { RegistrationAttendanceService } from './registration-attendance.service';
 import { RegistrationAttendance } from './entities/registration-attendance.entity';
 import { CreateRegistrationAttendanceInput } from './dto/create-registration-attendance.input';
@@ -12,6 +21,10 @@ import { UserType } from 'support/enums';
 import { GraphQLJSONObject } from 'graphql-type-json';
 import { RegistrationAttendancePage } from './entities/registration-attendance-page.entity';
 import { PaginateRegistrationAttendanceInput } from './dto/paginate-registration-attendance.input';
+import { CampRegistration } from 'src/camp-registration/entities/camp-registration.entity';
+import { DataloaderRegistry } from 'src/dataloaders/dataloaderRegistry';
+import { CampVariantRegistration } from 'src/camp-variant-registration/entities/camp-variant-registration.entity';
+import { Child } from 'src/child/entities/child.entity';
 
 @Resolver(() => RegistrationAttendance)
 // @UseGuards(GqlAuthGuard)
@@ -79,5 +92,47 @@ export class RegistrationAttendanceResolver {
     @Args('input') input: PaginateRegistrationAttendanceInput,
   ) {
     return this.service.paginate(input);
+  }
+
+  @ResolveField(() => CampRegistration, { nullable: true })
+  campRegistration(
+    @Parent() parent: RegistrationAttendance,
+    @Context() { loader }: { loader: DataloaderRegistry },
+  ) {
+    return loader.CampRegistrationDataLoader.load(parent.campRegistrationId);
+  }
+
+  @ResolveField(() => CampVariantRegistration, { nullable: true })
+  campVariantRegistration(
+    @Parent() parent: RegistrationAttendance,
+    @Context() { loaders }: { loaders: DataloaderRegistry },
+  ) {
+    return loaders.CampVariantsDataLoader.load(parent.campVariantId);
+  }
+
+  @ResolveField(() => Child, { nullable: true })
+  child(
+    @Parent() parent: RegistrationAttendance,
+    @Context() { loaders }: { loaders: DataloaderRegistry },
+  ) {
+    return loaders.ChildDataLoader.load(parent.childId);
+  }
+
+  @ResolveField(() => User, { nullable: true })
+  enterAuditor(
+    @Parent() parent: RegistrationAttendance,
+    @Context() { loaders }: { loaders: DataloaderRegistry },
+  ) {
+    return loaders.UsersDataLoader.load(parent.enterAuditorId);
+  }
+
+  @ResolveField(() => User, { nullable: true })
+  leaveAuditor(
+    @Parent() parent: RegistrationAttendance,
+    @Context() { loaders }: { loaders: DataloaderRegistry },
+  ) {
+    return parent.leaveAuditorId
+      ? loaders.UsersDataLoader.load(parent.leaveAuditorId)
+      : null;
   }
 }
