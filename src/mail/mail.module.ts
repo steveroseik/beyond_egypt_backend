31505b837cf3c81554a57bda568a4c42ab32b191
steveroseik <steveroseik@aucegypt.edu>
2025-06-10 19:10:05 +0300

    Integrates encryption module and updates GraphQL schema
    Adds the EncryptionModule to the mail module for enhanced security.
    Updates the GraphQL schema to make certain fields optional and modifies
    query structure to improve flexibility and data retrieval options.

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { MailService } from './mail.service';
+import { EncryptionModule } from 'src/encryption/encryption.module';
 
 @Module({
+  imports: [EncryptionModule],
   providers: [MailService],
   exports: [MailService],
 })
